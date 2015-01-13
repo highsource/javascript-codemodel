f52ae33656ffaff2c1f1086272b0613bb3737f79
highsource <aleksei.valikov@gmail.com>
2015-01-13 21:31:42 +0100

    Parser tests.

---
+++ parser/src/test/java/org/hisrc/jscm/parser/testing/AbstractDualParserTest.java
@@ -0,0 +1,40 @@
+package org.hisrc.jscm.parser.testing;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.hisrc.jscm.codemodel.JSProgram;
+import org.hisrc.jscm.codemodel.writer.CodeWriter;
+import org.hisrc.jscm.codemodel.writer.debug.CommentingCodeWriter;
+import org.hisrc.jscm.parser.EcmaScriptParser;
+import org.hisrc.jscm.parser.ParseException;
+import org.hisrc.jscm.parser.testing.util.AbstractDualResourceBasedTest;
+
+public class AbstractDualParserTest extends AbstractDualResourceBasedTest {
+
+	private EcmaScriptParser parse(String str) {
+		final Reader reader = new StringReader(str);
+		EcmaScriptParser parser = new EcmaScriptParser(reader);
+		parser.enable_tracing();
+		return parser;
+	}
+
+	@Override
+	protected void check(String sourceResourceName, String targetResourceName) {
+		final String source = getResourceAsString(sourceResourceName);
+		JSProgram program = null;
+		try {
+			program = parse(source).Program();
+			final CodeWriter codeWriter = new CommentingCodeWriter(System.out);
+			codeWriter.program(program);
+		} catch (ParseException pex) {
+			throw new AssertionError(pex);
+		}
+		catch(IOException ioex)
+		{
+			throw new AssertionError(ioex);
+		}
+	}
+
+}
