b9aee8b42c302ac66b5fd220f3a2fde02149f9ad
highsource <aleksei.valikov@gmail.com>
2015-02-10 07:20:48 +0100

    Check that octal literals start with 0.

--- parser/src/main/java/org/hisrc/jscm/parser/literal/OctalIntegerParser.java
+++ parser/src/main/java/org/hisrc/jscm/parser/literal/OctalIntegerParser.java
@@ -11,6 +11,8 @@ public class OctalIntegerParser implements TypedLiteralParser<BigInteger> {
 
 	public BigInteger parse(String string) throws ParseException {
 		Validate.notNull(string);
+		Validate.isTrue(string.length() > 1);
+		Validate.isTrue(string.substring(0, 1).equals("0"));
 		return new BigInteger(string, 8);
 	}
 }
