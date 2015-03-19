b61aec3d1e3acccc52e52c3bead403e2abd8f9f9
highsource <aleksei.valikov@gmail.com>
2015-03-19 09:13:40 +0100

    Started multiplicative expressions.

--- codemodel/src/main/java/org/hisrc/jscm/codemodel/writer/debug/CommentingExpressionWriter.java
+++ codemodel/src/main/java/org/hisrc/jscm/codemodel/writer/debug/CommentingExpressionWriter.java
@@ -201,8 +201,11 @@ public class CommentingExpressionWriter extends ExpressionWriter {
 	@Override
 	public CodeWriter visitMultiplicative(Multiplicative value)
 			throws IOException {
+		f.expression(value.getLeft());
 		f.inlineComment("E:Multiplicative");
-		return super.visitMultiplicative(value);
+		f.operator(value.getOperator());
+		f.expression(value.getRight());
+		return f;
 	}
 
 	@Override
