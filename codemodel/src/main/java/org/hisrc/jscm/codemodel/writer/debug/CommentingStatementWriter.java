7813dbbc74b606214a91df92742899ccd66c1717
highsource <aleksei.valikov@gmail.com>
2015-04-04 10:03:05 +0200

    Tests for the try statement.

--- codemodel/src/main/java/org/hisrc/jscm/codemodel/writer/debug/CommentingStatementWriter.java
+++ codemodel/src/main/java/org/hisrc/jscm/codemodel/writer/debug/CommentingStatementWriter.java
@@ -3,6 +3,7 @@ package org.hisrc.jscm.codemodel.writer.debug;
 import java.io.IOException;
 
 import org.hisrc.jscm.codemodel.statement.JSDebuggerStatement;
+import org.hisrc.jscm.codemodel.statement.JSTryStatement;
 import org.hisrc.jscm.codemodel.writer.CodeWriter;
 import org.hisrc.jscm.codemodel.writer.StatementWriter;
 
@@ -10,6 +11,29 @@ public class CommentingStatementWriter extends StatementWriter {
 
 	public CommentingStatementWriter(CodeWriter formatter) {
 		super(formatter);
+	}
+	
+	@Override
+	public CodeWriter visitTry(JSTryStatement value) throws IOException {
+		f.inlineComment("S:Try");
+		f.keyword("try").whiteSpace();
+		f.statement(value.getBody());
+		if (value.getCatch() != null) {
+			f.lineTerminator();
+			f.inlineComment("S:Try.Catch");
+			f.keyword("catch").whiteSpace();
+			f.openRoundBracket();
+			f.expression(value.getException());
+			f.closeRoundBracket().whiteSpace();
+			f.statement(value.getCatch());
+		}
+		if (value.getFinally() != null) {
+			f.lineTerminator();
+			f.inlineComment("S:Try.Finally");
+			f.keyword("finally").whiteSpace();
+			f.statement(value.getFinally());
+		}
+		return f;
 	}
 	
 	@Override
---
+++ parser/src/test/resources/org/hisrc/jscm/parser/statement/tests/TryStatementTest.00.js
@@ -0,0 +1,3 @@
+try {} catch (x) {} finally {}
+try {} catch (x) {}
+try {} finally {}
